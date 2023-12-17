6c9fc9f7ee0bdee59ca6dca81e5ba1ff43e1c2d9
justalexok <76539791+justalexok@users.noreply.github.com>
2023-12-17 00:28:51 +0000

    Base Character Setup

---
+++ Source/Poker/Poker.cpp
@@ -0,0 +1,6 @@
+// Copyright Alex Goulder
+
+#include "Poker.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Poker, "Poker" );
---
+++ Source/Poker/Poker.h
@@ -0,0 +1,6 @@
+// Copyright Alex Goulder
+
+#pragma once
+
+#include "CoreMinimal.h"
+
